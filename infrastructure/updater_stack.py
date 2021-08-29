7fb988ee9329ec1c3cedfbc0204899dc8e8bc7fc
gshpychka <glibshpychka@gmail.com>
2021-08-29 20:09:09 +0300

    secret read access

--- infrastructure/updater_stack.py
+++ infrastructure/updater_stack.py
@@ -3,6 +3,8 @@ from aws_cdk import (
     aws_lambda as lambda_,
     aws_events as events,
     aws_events_targets as events_targets,
+    aws_secretsmanager as sm,
+    aws_iam as iam,
 )
 import typing
 
@@ -13,14 +15,22 @@ class AwsCliPackageUpdaterStack(cdk.Stack):
 
         check_frequency = cdk.Duration.hours(1)
 
+        secret_name = "misc/ssh_keys/aur"
+        private_ssh_key = sm.Secret.from_secret_name_v2(
+            self, "ssh_key", secret_name=secret_name
+        )
+
         updater_lambda = lambda_.Function(
             self,
             "updater_fn",
             code=lambda_.Code.from_asset_image("src"),
+            environment={"SSH_KEY_SECRET_NAME": secret_name},
             handler=lambda_.Handler.FROM_IMAGE,
             runtime=lambda_.Runtime.FROM_IMAGE,
             timeout=cdk.Duration.seconds(10),
         )
+
+        private_ssh_key.grant_read(typing.cast(iam.Role, updater_lambda.role))
 
         event_target = events_targets.LambdaFunction(
             handler=typing.cast(lambda_.IFunction, updater_lambda),
